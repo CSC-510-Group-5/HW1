b1f269a538e8960e433ab9a7f2ef1da54f528f00
Mason Horne <masonhorne1@gmail.com>
2025-01-10 16:57:18 -0500

    Update hw1.py

--- hw1.py
+++ hw1.py
@@ -1,9 +1,15 @@
 import sys
 
-#number = sys.argv[1]
-#print(number)
 # A function to test evenness
 def is_even(num):
+    """
+    A function to check if a number is even
+        Args:
+        num (int): The number to check.
+
+    Returns:
+        boolean: Whether the number is even.
+    """
     if num % 2 == 0:
         return Null
     else:
