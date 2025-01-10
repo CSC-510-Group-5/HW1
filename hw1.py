be43425d19b9a4d50495d446488470a40e118445
Connorblu <63871157+Connorblu@users.noreply.github.com>
2025-01-10 20:25:11 +0000

    Adding buggy code

---
+++ hw1.py
@@ -0,0 +1,13 @@
+import sys
+
+number = sys.argv[1]
+print(number)
+# A function to test evenness
+def test_even(num):
+    if num % 2 == 0:
+        return Null
+    else:
+        return False
+    
+test_even(number)
+    
